f3fc525ca86bda537782d5bc0940efd8d1ef6c40
Carlos <carlosdanieltc@gmail.com>
2025-09-10 15:50:01 +0200

    Creados componentes Home, Pokedex y el enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PokedexComponent } from './pages/pokedex/pokedex.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pokedex', component: PokedexComponent },
+  { path: '**', redirectTo: '' } // cualquier ruta desconocida redirige a Home
+];
